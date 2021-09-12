96a9f9e37874249e02efeba3c3291e26db12285a
Yuichiro Makino <manhattan2020@gmail.com>
2021-09-12 16:48:18 +0900

    update

---
+++ dist/precache-manifest.30b506f50c0aa70a49adca5c4fdfd649.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef176c0a8d8e605ae2a8",
+    "url": "/css/about.a7bddf1e.css"
+  },
+  {
+    "revision": "c3f3823bb27ff4f3c84c",
+    "url": "/css/index.6ef0785c.css"
+  },
+  {
+    "revision": "ee226f966675f4545b23ae519e83e6d2",
+    "url": "/img/mark.ee226f96.svg"
+  },
+  {
+    "revision": "3717249db1920e77253c2a85bf2d51d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef176c0a8d8e605ae2a8",
+    "url": "/js/about.e8c2d5fe.js"
+  },
+  {
+    "revision": "7c4130a35952e2363c94",
+    "url": "/js/chunk-vendors.d6aeb8f3.js"
+  },
+  {
+    "revision": "c3f3823bb27ff4f3c84c",
+    "url": "/js/index.eb784ca4.js"
+  },
+  {
+    "revision": "c74d0d5ee3548ebbcdb153ee58663d04",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,10 +14,10 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9f39f3d142dcbfa2bcdd182a09da477.js"
+  "/precache-manifest.30b506f50c0aa70a49adca5c4fdfd649.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "vue202109"});
+workbox.core.setCacheNameDetails({prefix: "vue20210912"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
